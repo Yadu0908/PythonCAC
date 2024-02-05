7725f8cb63b8ecc0fc3ee4aef425f65900b8bb86
Yadu0908 <programming.with.yadu@gmail.com>
2024-02-05 15:15:47 +0000

    date commit

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
-print("Hello world")
+#here we can chnage our numbers into the digital number by using simple python methods
+
+
+# number = 128;
+# print(bin(number));
+# print(hex(number));
+# print(oct(number));
+
+
+
+import datetime
+today = datetime.datetime.now()
+ 
+# Prints readable format for date-time object
+print(str(today))
+ 
+# prints the official format of date-time object
+print(repr(today))
